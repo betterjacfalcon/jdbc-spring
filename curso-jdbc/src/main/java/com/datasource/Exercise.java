e03e8477f899a08f7cda6a77e077cbea41bf0e37
Alejandra Rodriguez <jacfalcon.better.practices@gmail.com>
2022-10-21 03:55:44 -0600

    Uso del pool de HikariCp

--- curso-jdbc/src/main/java/com/datasource/Exercise.java
+++ curso-jdbc/src/main/java/com/datasource/Exercise.java
@@ -3,23 +3,27 @@ package com.datasource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-import org.h2.jdbcx.JdbcConnectionPool;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 public class Exercise {
 
-	private static final int NEW_CONNECTIONS = 100000;
+	private static final int NEW_CONNECTIONS = 10000;
 
 	public static void main(String[] args) throws SQLException {
-		JdbcConnectionPool  connectionPool = JdbcConnectionPool.create("jdbc:h2:~/test", "postgres", "admin");
-
+		//JdbcConnectionPool  connectionPool = JdbcConnectionPool.create("jdbc:h2:~/test", "postgres", "admin");
+		HikariConfig config = new HikariConfig();
+		config.setJdbcUrl("jdbc:postgresql://localhost:5432/cure_illness");
+		config.setUsername("postgres");
+		config.setPassword("admin");
+		HikariDataSource connectionPool =new HikariDataSource(config);
 		long startTime = System.currentTimeMillis();
 		for(int i = 0; i< NEW_CONNECTIONS; i++) {
-			Connection connection = connectionPool.getConnection();
-			
-			
+			Connection connection = connectionPool.getConnection();			
 			connection.close();
 		}
-		System.out.println("Total Time= "+ ((System.currentTimeMillis() - startTime)/1000  + " s"));
+		connectionPool.close();
+		System.out.println("Total Time= "+ (System.currentTimeMillis() - startTime  + " ms"));
 
 	}
 
